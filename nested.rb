e261ab1d24a48adaca1e3ed986c6280d58bec9cc
Christoph Gruener <christophgruener@me.com>
2019-01-30 21:10:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -130,7 +130,7 @@ def adding_to_dennis
         }
      }
     
-programmer_hash[:dennis_ritchie][:languages].unshift("Assembly")
+programmer_hash[:dennis_ritchie][:languages].push("Assembly")
 programmer_hash
 
 end
